257a687e546547585d1ab2bf4cb7eae1218f4a5b
Gurnain_evon <Gurnain.singh@evontech.com>
2024-01-24 18:32:36 +0530

    commit for adding comments

--- index.js
+++ index.js
@@ -8,40 +8,40 @@ const io = socketIO(server, {
     cors: { origin: '*' }
 });
 
-let onlineUsers = [];
-let messageArray = [];
+let onlineUsers = []; // to set socketID for FE to understand which user is online
+let messageArray = []; // to add messages and room id of all the users
 
 const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
-    socket.on('join', (data) => {
-        socket.join(data.room);
-        socket.broadcast.to(data.room).emit('user joined', { user: data.user, status: 'online' });
-    });
+   /**
+   * @author: Gurnain
+   * To receive object containing messages from different users and send back triggering the 'new message' event
+   **/
     socket.on('message', (data) => {
-        console.log("kok", data);
         if(data){
             messageArray.push({ user: data.user, room: data.room, message: data.message, receiver: data.receiver , rId: data.rId})
         }
         io.emit('new message', messageArray);
       });
+         /**
+   * @author: Gurnain
+   * To receive object containing user details and send back triggering the 'get-users' event setting the online status
+   **/
     socket.on("new-user-add", (newUserId) => {
         if (!onlineUsers.some((user) => user.userId === newUserId)) {  
           onlineUsers.push({ userId: newUserId, socketId: socket.id , online: true});
-          console.log("new user is here!", onlineUsers);
         }
         io.emit("get-users", onlineUsers);
       });
       
       socket.on("disconnect", () => {
         onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id)
-        console.log("user disconnected", onlineUsers);
         io.emit("get-users", onlineUsers);
       });
       
       socket.on("offline", () => {
         onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id)
-        console.log("user is offline", onlineUsers);
         io.emit("get-users", onlineUsers);
       });
 });
